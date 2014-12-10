fbacee54718fb53b5e59f02b2d7a8a1e91195bae
Joshua Tauberer <jt@occams.info>
2014-12-10 17:36:36 -0500

    some design

--- itfsite/templatetags/itfsite_utils.py
+++ itfsite/templatetags/itfsite_utils.py
@@ -9,15 +9,36 @@ register = template.Library()
 @register.filter(is_safe=True)
 @stringfilter
 def render_text(value, format):
-    if str(format) in 'TextFormat.Markdown':
-        return safestring.mark_safe(markdown2.markdown(value, safe_mode=True))
-    return safestring.mark_safe(value)
+	if str(format) in 'TextFormat.Markdown':
+		return safestring.mark_safe(markdown2.markdown(value, safe_mode=True))
+	return safestring.mark_safe(value)
 
 @register.filter(is_safe=True)
 @stringfilter
 def markdown(value):
-    return safestring.mark_safe(markdown2.markdown(value, safe_mode=True))
+	return safestring.mark_safe(markdown2.markdown(value, safe_mode=True))
 
 @register.filter(is_safe=True)
 def json(value):
-    return safestring.mark_safe(jsonlib.dumps(value))
+	return safestring.mark_safe(jsonlib.dumps(value))
+
+@register.filter(is_safe=True)
+@stringfilter
+def currency(value):
+	# Make the value appear like currency. Rounds big numbers to
+	# whole-dollar amounts, so avoid this where exact amounts are
+	# important.
+	#
+	# We could use locale.currency(grouping=True), but we lose
+	# control over whether to show cents.
+
+	# value might be a Decimal instance.
+	value = float(value)
+
+	if value < 100:
+		# Show dollars and cents.
+		return "$%0.2f" % value
+
+	else:
+		# Only show dollars, but use commas to group digits.
+		return "${:,.0f}".format(value)
