c6aae3d51c5acef4857410922cccf8e6c6dd5254
Joshua Tauberer <jt@occams.info>
2014-12-31 13:14:06 -0500

    preliminary how-it-works (faq) page

--- itfsite/urls.py
+++ itfsite/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns('',
 	url(r'^$', 'itfsite.views.homepage', name='homepage'),
-	url(r'^(about|about/theory-of-change|privacy|legal|open)$', 'itfsite.views.simplepage', name='simplepage'),
+	url(r'^(about|about/how-it-works|about/theory-of-change|privacy|legal|open)$', 'itfsite.views.simplepage', name='simplepage'),
 	url(r'^accounts/welcome$', 'itfsite.accounts.welcome', name='welcome'),
 	url(r'^home$', 'itfsite.views.user_home', name='home'),
 
