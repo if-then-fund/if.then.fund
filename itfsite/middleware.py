24cd0ccfb5f63f2379ca2dc97a5911c630cbcdea
Joshua Tauberer <jt@occams.info>
2015-02-08 00:37:41 -0500

    move the mixpanel account identifier into the environment file and if it is not set then disable mixpanel so that we dont record events from test servers

--- itfsite/middleware.py
+++ itfsite/middleware.py
@@ -4,4 +4,5 @@ def itfsite_template_context_processor(request):
 	return {
 		"SITE_MODE": settings.SITE_MODE,
 		"ROOT_URL": request.build_absolute_uri("/"),
+		"MIXPANEL_ID": settings.MIXPANEL_ID,
 	}
--- itfsite/settings.py
+++ itfsite/settings.py
@@ -143,4 +143,5 @@ SITE_MODE = environment.get("mode")
 SITE_ROOT_URL = EMAIL_CONFIRM_LA_HTTP_PROTOCOL + "://" + environment['host']
 DE_API = environment['democracyengine']
 CDYNE_API_KEY = environment['cdyne_key']
+MIXPANEL_ID = environment.get('mixpanel_id')
 CURRENT_ELECTION_CYCLE = 2016
