63a0207345c7ef2c98440113f66dea84dd7e878c
Joshua Tauberer <jt@occams.info>
2015-03-17 09:13:42 -0400

    revise 'psql' utility management command to let me call pg_dump too

--- itfsite/management/commands/psql.py
+++ itfsite/management/commands/psql.py
@@ -3,6 +3,7 @@
 
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
+from optparse import make_option
 
 import os
 from urllib.parse import urlencode
@@ -11,8 +12,19 @@ class Command(BaseCommand):
 	args = ''
 	help = 'Start psql with database settings.'
 
+	option_list = BaseCommand.option_list + (
+		make_option('--pg_dump',
+			action='store_true',
+			dest='pg_dump',
+			default=False,
+			help='Run pg_dump instead of psql.'),
+		)
+
 	def handle(self, *args, **options):
 		d = settings.DATABASES['default']
+		if d['ENGINE'] != 'django.db.backends.postgresql_psycopg2':
+			print("The default database is not a PostgreSQL database..")
+			return
 
 		# build a connection string so that we can specify sslmode
 		conn = "postgresql://%s:%s@%s:%s/%s" % (
@@ -20,13 +32,18 @@ class Command(BaseCommand):
 		if d.get('OPTIONS', {}):
 			conn += "?" + urlencode(d['OPTIONS'])
 
-		# args
-		psqlargs = ['psql', conn]
-		if len(args) > 0:
-			psqlargs.append('-c')
-			psqlargs.append(" ".join(args))
+		# binary & args
+		if not options['pg_dump']:
+			binary = 'psql'
+			psqlargs = [binary, conn]
+			if len(args) > 0:
+				psqlargs.append('-c')
+				psqlargs.append(" ".join(args))
+		else:
+			binary = 'pg_dump'
+			psqlargs = [binary, conn] + list(args)
 		
 		# replace this process with psql
 		print(" ".join(psqlargs))
-		os.execv('/usr/bin/psql', psqlargs)
+		os.execv('/usr/bin/' + binary, psqlargs)
 
