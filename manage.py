e7723a7c4a0473f38a0e0231f362e472491690c2
Joshua Tauberer <jt@occams.info>
2015-03-31 12:15:43 +0000

    when management commands are run from a non-tty, mail exceptions to the site admins

--- manage.py
+++ manage.py
@@ -8,4 +8,26 @@ if __name__ == "__main__":
 
 	from django.core.management import execute_from_command_line
 
-	execute_from_command_line(sys.argv)
+	if sys.stderr.isatty():
+		# When management commands are run from an interactive shell,
+		# run the function directly.
+		execute_from_command_line(sys.argv)
+	else:
+		# We're running from a cron job or other script. Mail exceptions
+		# to the administrator in the same way that live exceptions on
+		# the site get mailed to admins.
+
+		from django.utils.log import AdminEmailHandler
+		import logging
+		logger = logging.getLogger('django_management_command')
+		logger.addHandler(AdminEmailHandler())
+
+		try:
+			execute_from_command_line(sys.argv)
+
+		except Exception as e:
+			# email admins
+			logger.error('Admin Command Error: %s', ' '.join(sys.argv), exc_info=sys.exc_info())
+
+			# also write to stderr & exit
+			raise
