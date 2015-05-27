32dcc0e1c95ebf4991518fcbae7d458c735fcde2
Joshua Tauberer <jt@occams.info>
2015-05-27 08:36:49 -0400

    change the ROOT_URL global template variable to not have a trailing slash

--- itfsite/middleware.py
+++ itfsite/middleware.py
@@ -3,7 +3,7 @@ from django.conf import settings
 def itfsite_template_context_processor(request):
 	return {
 		"SITE_MODE": settings.SITE_MODE,
-		"ROOT_URL": request.build_absolute_uri("/"),
+		"ROOT_URL": request.build_absolute_uri("/").strip("/"), # remove trailing slash
 		"MIXPANEL_ID": settings.MIXPANEL_ID,
 	}
 
