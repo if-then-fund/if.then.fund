if-then-fund/if.then.fund

commit 5121e179d3900142566fa37e6ccbb08219a1f3a4

show a campaign's totals on the homepage, campaign page, and organization page listing of campaigns

contrib/models.py
@@ -335,7 +335,7 @@ class TriggerCustomization(models.Model):
 		unique_together = ('trigger', 'owner')
 
 	def __str__(self):
-		return "%s / %s: %s" % (self.owner, self.trigger, self.title)
+		return "%s / %s" % (self.owner, self.trigger)
 
 	def has_fixed_outcome(self):
 		return self.outcome is not None
@@ -1328,7 +1328,8 @@ class Contribution(models.Model):
 			{ "trigger_execution": self.action.execution, "via_campaign": self.pledge_execution.pledge.via_campaign },
 		]:
 			update(**kw)
-			update(outcome=self.pledge_execution.pledge.desired_outcome, **kw)
+			if "trigger_execution" in kw:
+				update(outcome=self.pledge_execution.pledge.desired_outcome, **kw)
 			update(actor=self.action.actor, incumbent=not self.recipient.is_challenger, **kw)
 			update(incumbent=not self.recipient.is_challenger, **kw)
 			update(party=self.action.party if not self.recipient.is_challenger else self.recipient.party, **kw)
@@ -1364,7 +1365,7 @@ class ContributionAggregate(models.Model):
 		unique_together = [CONTRIBUTION_AGGREGATE_FIELDS]
 
 	def __str__(self):
-		return "%d | %s %s %s %s %s %s" % (self.trigger_execution_id, self.via, self.outcome, self.action_id, self.incumbent, self.party, self.district)
+		return "%d | %s %s %s %s %s %s" % (self.trigger_execution_id, self.via_campaign, self.outcome, self.actor_id, self.incumbent, self.party, self.district)
 
 	class Updater(object):
 		def __init__(self, buffered=True):
@@ -1447,10 +1448,31 @@ class ContributionAggregate(models.Model):
 		# if actors were requested, pre-fetch objects
 		actors = Actor.objects.in_bulk(rec['actor'] for rec in ret if 'actor' in rec)
 
+		# if actors were requested add Action instances
+		actions = { }
+		if slce.get('trigger_execution') and len(actors) > 0:
+			actions = Action.objects.filter(execution=slce['trigger_execution']).select_related('execution__trigger')
+			actions = { action.actor_id: action for action in actions }
+
+		# if outcome was requested, add outcome labels
+		if 'outcome' in across and slce.get('trigger_execution'):
+			outcome_strings = slce['trigger_execution'].trigger.outcome_strings()
+			if slce.get('via_campaign'):
+				# Get outcome strings from any TriggerCustomization, if there is one.
+				tcust = TriggerCustomization.objects.filter(owner=slce['via_campaign'].owner, trigger=slce['trigger_execution'].trigger).first()
+				if tcust:
+					outcome_strings = tcust.outcome_strings()
+
 		# turn integers from .values() back into objects
 		for rec in ret:
-			if 'actor' in rec: rec['actor'] = actors[rec['actor']]
-			if 'party' in rec: rec['party'] = ActorParty(rec['party'])
+			if 'actor' in rec:
+				rec['actor'] = actors[rec['actor']]
+				rec['action'] = actions[rec['actor'].id]
+
+			if 'party' in rec:
+				rec['party'] = ActorParty(rec['party'])
+			if 'outcome' in rec and 'trigger_execution' in slce:
+				rec['label'] = outcome_strings[rec['outcome']]['label']
 		
 		return ret
 

contrib/views.py
@@ -390,19 +390,22 @@ def cancel_pledge(request):
 
 @anonymous_view
 def report(request):
-	context = {
+	if "trigger" in request.GET:
+		trigger = get_object_or_404(Trigger, id=request.GET['trigger'])
+	else:
+		trigger = None
 
+	context = {
 	}
-	context.update(report_fetch_data(request))
+	context.update(report_fetch_data(trigger))
 	return render(request, "contrib/totals.html", context)
 
-def report_fetch_data(request):
+def report_fetch_data(trigger, via_campaign, with_actor_details=True):
 	pledge_slice_fields = { }
-	slice_fields = { }
+	pledgeexec_slice_fields = { }
+	ca_slice_fields = { }
 
-	if "trigger" in request.GET:
-		# get the object & validate that there is data
-		trigger = get_object_or_404(Trigger, id=request.GET['trigger'])
+	if trigger:
 		pledge_slice_fields["trigger"] = trigger
 		try:
 			te = trigger.execution
@@ -412,7 +415,13 @@ def report_fetch_data(request):
 			raise Http404("This trigger did not have any contributions.")
 		if te.pledge_count < .75 * trigger.pledge_count:
 			raise Http404("This trigger is still being executed.")
-		slice_fields["trigger_execution"] = te
+		pledgeexec_slice_fields["trigger_execution"] = te
+		ca_slice_fields["trigger_execution"] = te
+
+	if via_campaign:
+		pledge_slice_fields["via_campaign"] = via_campaign
+		pledgeexec_slice_fields["pledge__via_campaign"] = via_campaign
+		ca_slice_fields["via_campaign"] = via_campaign
 
 	# form response
 	ret = { }
@@ -427,34 +436,37 @@ def report_fetch_data(request):
 	ret["pledge_aggregate"] = pledges.aggregate(amount=Sum('amount'))["amount"]
 
 	# number of executed pledges and users with executed pledges
-	pledge_executions = PledgeExecution.objects.filter(problem=PledgeExecutionProblem.NoProblem, **slice_fields)
+	pledge_executions = PledgeExecution.objects.filter(problem=PledgeExecutionProblem.NoProblem, **pledgeexec_slice_fields)
 	ret["users"] = pledge_executions.values("pledge__user").distinct().count()
 	ret["num_triggers"] = pledge_executions.values("trigger_execution").distinct().count()
 	ret["first_contrib_date"] = pledge_executions.order_by('created').first().created
 	ret["last_contrib_date"] = pledge_executions.order_by('created').last().created
 
 	# aggregate count and amount of campaign contributions
-	ret["total"] = ContributionAggregate.get_slice(**slice_fields)
+	ret["total"] = ContributionAggregate.get_slice(**ca_slice_fields)
 	if ret["total"]["count"] > 0:
 		ret["total"]["average"] = ret["total"]["total"] / ret["total"]["count"]
 
-	if "trigger_execution" in slice_fields:
+	if "trigger_execution" in ca_slice_fields:
 		# Aggregates by outcome.
-		ret['outcomes'] = ContributionAggregate.get_slices('outcome', **slice_fields)
+		ret['outcomes'] = ContributionAggregate.get_slices('outcome', **ca_slice_fields)
 
 	# Aggregates by actor.
-	from collections import defaultdict
-	ret['actors'] = defaultdict(lambda : defaultdict( lambda : decimal.Decimal(0) ))
-	for rec in ContributionAggregate.get_slices('actor', 'incumbent', **slice_fields):
-		ret['actors'][rec['actor']]['actor'] = rec['actor']
-		ret['actors'][rec['actor']][str(rec['incumbent'])] += rec['total']
-	ret['actors'] = sorted(ret['actors'].values(), key = lambda x : (-(x['True'] + x['False']), -x['True'], x['actor'].name_sort))
+	if with_actor_details:
+		from collections import defaultdict
+		ret['actors'] = defaultdict(lambda : defaultdict( lambda : decimal.Decimal(0) ))
+		for rec in ContributionAggregate.get_slices('actor', 'incumbent', **ca_slice_fields):
+			ret['actors'][rec['actor']]['actor'] = rec['actor']
+			ret['actors'][rec['actor']][str(rec['incumbent'])] += rec['total']
+			if "action" in rec:
+				ret['actors'][rec['actor']]['action'] = rec['action']
+		ret['actors'] = sorted(ret['actors'].values(), key = lambda x : (-(x['True'] - x['False']), -x['True'], x['actor'].name_sort))
 
 	# Aggregates by incumbent/chalenger.
-	ret['by_incumb_chlngr'] = ContributionAggregate.get_slices('incumbent', **slice_fields)
+	ret['by_incumb_chlngr'] = ContributionAggregate.get_slices('incumbent', **ca_slice_fields)
 
 	# Aggregates by party.
-	ret['by_party'] = ContributionAggregate.get_slices('party', **slice_fields)
+	ret['by_party'] = ContributionAggregate.get_slices('party', **ca_slice_fields)
 
 	# report
 	return ret

itfsite/models.py
@@ -4,6 +4,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.utils import timezone
 from django.template import Template, Context
 from django.conf import settings
+from django.http import Http404
 
 from itfsite.accounts import User, NotificationsFrequency
 from contrib.models import TextFormat
@@ -109,6 +110,89 @@ class Campaign(models.Model):
 
 	def get_short_url(self):
 		return settings.SITE_ROOT_URL + ("/a/%d" % self.id)
+
+	def get_contrib_totals(self):
+		# Get all of the displayable totals for this campaign.
+
+		ret = { }
+
+		from django.db.models import Sum, Count
+		from contrib.models import TriggerStatus, TriggerCustomization, Pledge, PledgeExecution, PledgeExecutionProblem
+
+		# What pledges should we show? For consistency across stats, filter out unconfirmed
+		# pledges.
+		pledges = Pledge.objects.exclude(user=None)
+		if self.owner:
+			# When we're showing a campaign owned by an organization, then we
+			# only count pledges to this very campaign.
+			pledges = pledges.filter(via_campaign=self)
+		else:
+			# Otherwise, we can count any campaign but only to triggers in this
+			# campaign.
+			pledges = pledges.filter(trigger__in=self.contrib_triggers.all())
+
+		# If no trigger cutomization has a fixed outcome, then we can show
+		# plege totals. (We can't show pledge totals when there is a fixed
+		# outcome.) In no case do we break this down by desired outcome.
+		# There are never TriggerCustomizations when this campaign has no
+		# owner.
+		tcusts = TriggerCustomization.objects.filter(owner=self.owner, trigger__campaigns=self)
+		if not tcusts.exclude(outcome=None).exists():
+			ret["pledged_total"] = pledges.aggregate(sum=Sum('amount'))["sum"] or 0
+			ret["pledged_user_count"] = pledges.values("user").distinct().aggregate(count=Count('user'))["count"] or 0
+		else:
+			ret["pledged_site_wide"] = Pledge.objects.exclude(user=None).filter(trigger__in=self.contrib_triggers.all()).aggregate(sum=Sum('amount'))["sum"] or 0
+
+		# If any trigger has been executed, then we can show executed totals.
+		# In all cases we can show the total amount of contributions across all triggers.
+		# Of course, this could be on all sides of an issue, so this isn't usually
+		# interesting.
+		ret["contrib_total"] = 0
+		ret["contrib_user_count"] = 0 # not distinct across triggers
+
+		# Assume all fixed-outcome triggers are about the same issue. Compute the totals
+		# across those triggers. Otherwise, we don't know whether outcome X in any trigger
+		# corresponds to the same real-world issue as outcome X in any other trigger.
+		ret["contrib_fixed_outcome_total"] = 0
+
+		# Report outcomes by trigger, with a breakdown by outcome, and sum across triggers.
+		from contrib.views import report_fetch_data
+		ret["by_trigger"] = []
+		for trigger in self.contrib_triggers.filter(status=TriggerStatus.Executed).order_by('-created'):
+			try:
+				# Get this trigger's totals.
+				agg = report_fetch_data(trigger, via_campaign=self if self.owner else None)
+				ret["by_trigger"].append({
+					"trigger": trigger,
+					"aggregates": agg
+				})
+
+				# We sum here and not in an aggregate SQL statement for two reasons:
+				# Triggers that haven't had all of their pledges executed should not
+				# reveal grossly incomplete information. And our templates assume that
+				# if contrib_total > 0, then there is by_trigger information. So we
+				# do these two parts together for consistency.
+				ret["contrib_total"] += agg["total"]["total"]
+				ret["contrib_user_count"] += agg["users"] # not distinct
+
+				# If this trigger has a TriggerCustomization with a fixed outcome,
+				# sum the total contributions for that outcome only.
+				tcust = TriggerCustomization.objects.filter(owner=self.owner, trigger=trigger).first()
+				if tcust and tcust.outcome is not None:
+					for outcome in agg["outcomes"]:
+						if outcome["outcome"] == tcust.outcome:
+							# No easy way to get the total number of unique users.
+							ret["contrib_fixed_outcome_total"] += outcome["total"]
+
+			except Http404:
+				# This is how report_fetch_data indicates that data
+				# is not available. That could be because we haven't
+				# yet executed enough pledges to report contribution
+				# totals.
+				continue
+
+		return ret
+
 
 #####################################################################
 #

itfsite/views.py
@@ -14,7 +14,7 @@ def homepage(request):
 	# The site homepage.
 
 	return render(request, "itfsite/homepage.html", {
-		"open_campaigns": Campaign.objects.filter(status=CampaignStatus.Open).order_by('-created')[0:12],
+		"open_campaigns": Campaign.objects.filter(status=CampaignStatus.Open).order_by('-created')[0:16], # 16 for debugging
 	})
 
 def simplepage(request, pagename):
@@ -167,19 +167,6 @@ def campaign(request, id):
 		return redirect(campaign.get_absolute_url()+qs)
 
 	from contrib.models import TriggerStatus, TriggerCustomization, Pledge
-	from django.db.models import Sum, Count
-
-	# Load all of the TriggerCustomizations that apply to any of the triggers.
-	triggers = [
-		(trigger, TriggerCustomization.objects.filter(owner=campaign.owner, trigger=trigger).first())
-		for trigger in campaign.contrib_triggers.all()
-	]
-
-	# We can only show pledged totals if no triggers have customizations that restrict the outcome.
-	can_show_pledge_totals = True
-	for trigger, tcust in triggers:
-		if tcust and tcust.outcome:
-			can_show_pledge_totals = False
 
 	# What trigger should the user take action on?
 	trigger = campaign.contrib_triggers.filter(status__in=(TriggerStatus.Open,TriggerStatus.Executed)).order_by('-created').first()
@@ -190,13 +177,14 @@ def campaign(request, id):
 	# render page
 	return render(request, "itfsite/campaign.html", {
 		"campaign": campaign,
+
+		# for contrib.Trigger actions
 		"trigger": trigger,
 		"tcust": tcust,
 		"trigger_outcome_strings": tcust.outcome_strings() if tcust else trigger.outcome_strings(),
 		"suggested_pledge": 5,
 		"alg": Pledge.current_algorithm(),
-		"pledged_total": Pledge.objects.filter(via_campaign=campaign).exclude(user=None).aggregate(sum=Sum('amount'))["sum"] if can_show_pledge_totals else None,
-		"pledged_user_count": Pledge.objects.filter(via_campaign=campaign).exclude(user=None).values("user").aggregate(count=Count('user'))["count"] if can_show_pledge_totals else None,
+
 		})
 	
 @user_view_for(campaign)

fixtures/create-random-pledge
@@ -5,7 +5,7 @@ from contrib.models import *
 
 campaign = Campaign.objects.get(id=sys.argv[1])
 trigger = campaign.contrib_triggers.first()
-user = User.objects.create(email="unit.test+%d@if.then.fund" % random.randint(10000, 20000))
+user = User.objects.create(email="unit.test+%d@if.then.fund" % random.randint(100000, 900000))
 ci = ContributorInfo.createRandom()
 p = Pledge.objects.create(
 	user=user,

fixtures/create-test-data
@@ -5,30 +5,35 @@
 # a circular dependency
 ./manage.py loaddata fixtures/actor.yaml fixtures/recipient.yaml
 
-# Create some sample triggers.
+# Create:
+#   4 triggers (1..4)
+#   3 organizations
+#   16 campaigns:
+#     * 1-4 is trigger 1, 5-8 is trigger 2, 9-12 is trigger 3, 13-16 is trigger 4
+#     * 1, 5, 9, and 13 have no owner.
+#     * 2, 6, 10, 14 are for org1 and have a TriggerCustomization
+#     * 3, 7, 11, 15 are for org2 and have a TriggerCustomization with a fixed outcome.
+#     * 4, 8, 12, 16 are for org3 but with no TriggerCustomization.
 fixtures/sample-triggers 
 
-# Create a bunch of random pledges on campaigns 1 and 2.
-fixtures/create-random-pledge 1
-fixtures/create-random-pledge 1
-fixtures/create-random-pledge 1
-fixtures/create-random-pledge 1
-fixtures/create-random-pledge 1
-fixtures/create-random-pledge 1
-fixtures/create-random-pledge 1
-fixtures/create-random-pledge 1
-fixtures/create-random-pledge 2
-fixtures/create-random-pledge 2
-fixtures/create-random-pledge 2
-fixtures/create-random-pledge 2
-fixtures/create-random-pledge 2
-fixtures/create-random-pledge 2
-fixtures/create-random-pledge 2
-fixtures/create-random-pledge 2
+# Create a bunch of random pledges on triggers 1-2 (campaigns 1 through 8).
+for c in {1..8}; do
+	for x in {1..8}; do
+		fixtures/create-random-pledge $c
+	done
+done
 
-# Execute triggers 2 and 3.
-fixtures/execute-trigger 2
-fixtures/execute-trigger 3
+# Execute triggers 2 through 4.
+for c in {2..4}; do
+	fixtures/execute-trigger $c
+done
+
+# Create additional post-execution pledges on triggers 3 and 4 (campaigns 9 through 16).
+for c in {9..16}; do
+	for x in {1..4}; do
+		fixtures/create-random-pledge $c
+	done
+done
 
 # Do what we need to do before we can execute pledges.
 ./manage.py send_pledge_emails
@@ -36,3 +41,14 @@ fixtures/execute-trigger 3
 # Execute pledges. Since we made up billing tokens we
 # disable Democracy Engine here.
 NO_DE=1 ./manage.py execute_pledges
+
+# Create additional unexecuted post-execution pledges on trigger 4 (campaigns 13 through 16).
+for c in {13..16}; do
+	for x in {1..4}; do
+		fixtures/create-random-pledge $c
+	done
+done
+
+# Make a superuser if one doesn't already exist.
+./manage.py createsuperuser --email=ops@if.then.fund --noinput
+echo "from itfsite.models import User; u = User.objects.get(email='ops@if.then.fund'); u.set_password('1234'); u.save()" | ./manage.py shell

fixtures/sample-triggers
@@ -1,10 +1,11 @@
 #!django_script
+import random
 from itfsite.models import *
 from contrib.models import *
 from contrib.legislative import create_trigger_from_bill, TriggerAlreadyExistsException
 
 # Create some triggers.
-for bill in ("hconres1-114", "hr22-114", "s30-114"):
+for bill in ("hconres1-114", "hr22-114", "s30-114", "s31-114"):
 	try:
 		t = create_trigger_from_bill(bill, 'h')
 		t.status = TriggerStatus.Open
@@ -12,29 +13,53 @@ for bill in ("hconres1-114", "hr22-114", "s30-114"):
 	except TriggerAlreadyExistsException as e:
 		print(bill, e)
 
-# Create an Organization to own some Campaigns and TriggerCustomizations.
-org = Organization.objects.filter(name="Test Organization").first()
-if not org:
-	org = Organization.objects.create(
-		name="Test Organization",
-		slug="test-organization",
+# Create some Organizations to own some Campaigns and TriggerCustomizations.
+org1 = Organization.objects.filter(name="Test Organization 1").first()
+if not org1:
+	org1 = Organization.objects.create(
+		name="Test Organization 1",
+		slug="test-organization-1",
+		orgtype=OrganizationType.C4,
+		description="This is a test organization.",
+		description_format=0,
+		)
+org2 = Organization.objects.filter(name="Test Organization 2").first()
+if not org2:
+	org2 = Organization.objects.create(
+		name="Test Organization 2",
+		slug="test-organization-2",
+		orgtype=OrganizationType.C4,
+		description="This is a test organization.",
+		description_format=0,
+		)
+org3 = Organization.objects.filter(name="Test Organization 3").first()
+if not org3:
+	org3 = Organization.objects.create(
+		name="Test Organization 3",
+		slug="test-organization-3",
 		orgtype=OrganizationType.C4,
 		description="This is a test organization.",
 		description_format=0,
 		)
 
-# Create a triggercustomization for each.
+# Create TriggerCustomizations for each...
 for trigger in Trigger.objects.all():
 	TriggerCustomization.objects.get_or_create(
-		owner=org,
+		owner=org1,
 		trigger=trigger,
 		defaults={
 
 		})
+	TriggerCustomization.objects.get_or_create(
+		owner=org2,
+		trigger=trigger,
+		defaults={
+			outcome=random.randint(0, 1),
+		})
 
 # Create a campaign for each.
 for trigger in Trigger.objects.all():
-	for owner in (None, org):
+	for owner in (None, org1, org2, org3):
 		campaign = Campaign.objects.filter(owner=owner, contrib_triggers=trigger).first()
 		if not campaign:
 			campaign = Campaign.objects.create(

itfsite/templates/itfsite/campaign.html
@@ -116,6 +116,10 @@ form span.required:after {
 	background-color: #EEE;
 	padding-top: 2em;
 }
+
+.table {
+	font-size: 90%;
+}
 </style>
 {% endblock %}
 
@@ -175,17 +179,63 @@ form span.required:after {
 			{% endwith %}
 		</div>
 		<div class="col-sm-4">
+			{% with totals=campaign.get_contrib_totals %}
 			<dl>
 				{% if letters_campaign %}
 				<dt>###</dt>
 				<dd>letters written</dd>
 				{% endif %}
 
-				{% if pledged_total %}
-					<dt>${{pledged_total}}</dt>
-					<dd>committed so far by {{pledged_user_count|intcomma}} contributor{{pledged_user_count|pluralize}}</dd>
+				{% if totals.contrib_total > 0 %}
+					{# has contributions #}
+					{% if totals.contrib_fixed_outcome_total > 0 %}
+						<dt>{{totals.contrib_fixed_outcome_total|currency}}</dt>
+						<dd>
+							contributed by {{totals.contrib_user_count|intcomma}} contributor{{totals.contrib_user_count|pluralize}}
+							{% if totals.by_trigger|length == 1 %}
+							<small>(<a href="#" onclick="$('#modal_trigger_{{totals.by_trigger.0.trigger.id}}').modal(); return false;">details</a>)</small>
+							{% endif %}
+						</dd>
+						{% if totals.by_trigger|length > 1 %}
+						{% for trigger in totals.by_trigger %}
+							<p><small><a href="#" onclick="$('#modal_trigger_{{trigger.trigger.id}}').modal(); return false;">{{trigger.trigger.title}}</a></small></p>
+						{% endfor %}
+						{% endif %}
+					
+					{% else %}
+						{% for trigger in totals.by_trigger %}
+							{% if totals.by_trigger|length > 1 %}
+								<p><b>{{trigger.trigger.title}}</b></p>
+							{% endif %}
+							<p style="float: right"><small><a href="#" onclick="$('#modal_trigger_{{trigger.trigger.id}}').modal(); return false;">Details</a></small></p>
+							{% for outcome in trigger.aggregates.outcomes %}
+								<dt>{{outcome.total|currency}}</dt>
+								<dd>{{outcome.label}}</dd>
+							{% endfor %}
+						{% endfor %}
+					{% endif %}
+
+				{% elif totals.pledged_total != None %}
+					{# just pledges, no contributions yet #}
+					<dt>{{ totals.pledged_total|currency }}</dt>
+					<dd>committed so far by {{totals.pledged_user_count|intcomma}} contributor{{totals.pledged_user_count|pluralize}}</dd>
+
+				{% else %}
+					<dt>{{ totals.pledged_site_wide|currency }}</dt>
+					<dd>committed on all sides of this issue site-wide*</dd>
+					<p style="font-size: 13px; line-height: 128%;">*Federal law prevents us from giving this campaign&rsquo;s total ahead of congressional action. Instead we show the total committed on both sides of this issue across all <span class="site-brand">if.then.fund</span> campaigns.</p>
+
 				{% endif %}
+
 			</dl>
+			{% endwith %}
+
+			<hr>
+
+			<small>
+			<p>This page was added to <span class="site-brand">if.then.fund</span> {% if campaign.owner %}by <a href="{{campaign.owner.get_absolute_url}}">{{campaign.owner.name}}</a>{% endif %} on {{campaign.created|date}}.</p>
+			{% if trigger.execution %}<p>The {{trigger.trigger_type.strings.action_noun}} occurred on {{trigger.execution.action_time|date}}. Contributions began to be processed on {{trigger.execution.created|date}}.</p>{% endif %}
+			</small>
 		</div>
 	</div>
 </div>
@@ -327,6 +377,57 @@ instructions. More on <a href="/about/how-it-works">how it works</a>.</p>
 	<div style="margin-top: .5em"><input id="utm_tool_link" type="text" value="" style="width: 100%" readonly></div>
 </div>
 
+{% with totals=campaign.get_contrib_totals %}
+{% for trigger in totals.by_trigger %}
+    <div id="modal_trigger_{{trigger.trigger.id}}" class="modal" tabindex="-1" role="dialog" aria-labelledby="modal_trigger_{{trigger.trigger.id}}_title" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4 class="modal-title" id="modal_trigger_{{trigger.trigger.id}}_title">{{trigger.trigger.title}}</h4>
+          </div>
+          <div class="modal-body">
+          	{{trigger.trigger.execution.description|render_text:trigger.trigger.execution.description_format}}
+
+            <h5 style="margin-top: 2em">Contributions By {{trigger.trigger.trigger_type.strings.action_noun.title}}</h5>
+            <table class="table" style="width: auto">
+            <tr><th>{{trigger.trigger.trigger_type.strings.action_noun.title}}</th> <th>Total Contributions</th></tr>
+            {% for outcome in trigger.aggregates.outcomes %}
+            	<tr><td>{{outcome.label}}</td> <td>{{outcome.total|currency}}</td></tr>
+            {% endfor %}
+            </table>
+
+            <h5>Contributions By Party of Recipient Candidate</h5>
+            <table class="table" style="width: auto">
+            <tr><th>Party</th> <th>Total Contributions</th></tr>
+            {% for party in trigger.aggregates.by_party %}
+            	<tr><td>{{party.party.name}}</td> <td>{{party.total|currency}}</td></tr>
+            {% endfor %}
+            </table>
+
+            <h5>Contributions By {{trigger.trigger.trigger_type.strings.actor.title}}</h5>
+            <table class="table">
+            <tr>
+            	<th>{{trigger.trigger.trigger_type.strings.actor.title}}</th>
+            	<th>{{trigger.trigger.trigger_type.strings.action_noun.title}}</th>
+            	<th>Contributions to<br>{{trigger.trigger.trigger_type.strings.actor.title}}</th>
+            	<th>Contributions to<br>Opponent</th>
+            </tr>
+            {% for actor in trigger.aggregates.actors %}
+            	<tr>
+            		<td>{{actor.actor}}</td>
+            		<td>{{actor.action.outcome_label}}</td>
+            		<td class="{% if actor.True > 0 %}text-success{% else %}text-muted{% endif %}">{{actor.True|currency}}</td>
+            		<td class="{% if actor.False > 0 %}text-danger{% else %}text-muted{% endif %}">{{actor.False|currency}}</td>
+            	</tr>
+            {% endfor %}
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+{% endfor %}
+{% endwith %}
 
 {# with body-wide, we leave open two divs: an unstyled dive and a .container #}
 

commit a9de8afd30f6617a2abbe5338f5ed8ccef3f3e24

5121e179d3900142566fa37e6ccbb08219a1f3a4 broke the totals page

contrib/models.py
@@ -1543,11 +1543,12 @@ class ContributionAggregate(models.Model):
 		for rec in ret:
 			if 'actor' in rec:
 				rec['actor'] = actors[rec['actor']]
-				rec['action'] = actions[rec['actor'].id]
+				if slce.get('trigger_execution'):
+					rec['action'] = actions[rec['actor'].id]
 
 			if 'party' in rec:
 				rec['party'] = ActorParty(rec['party'])
-			if 'outcome' in rec and 'trigger_execution' in slce:
+			if 'outcome' in rec and slce.get('trigger_execution'):
 				rec['label'] = outcome_strings[rec['outcome']]['label']
 		
 		return ret

contrib/views.py
@@ -428,14 +428,8 @@ def render_pledge_template(request, pledge, campaign, show_long_title=False):
 
 @anonymous_view
 def report(request):
-	if "trigger" in request.GET:
-		trigger = get_object_or_404(Trigger, id=request.GET['trigger'])
-	else:
-		trigger = None
-
-	context = {
-	}
-	context.update(report_fetch_data(trigger))
+	context = { }
+	context.update(report_fetch_data(None, None))
 	return render(request, "contrib/totals.html", context)
 
 def report_fetch_data(trigger, via_campaign, with_actor_details=True):
@@ -477,8 +471,9 @@ def report_fetch_data(trigger, via_campaign, with_actor_details=True):
 	pledge_executions = PledgeExecution.objects.filter(problem=PledgeExecutionProblem.NoProblem, **pledgeexec_slice_fields)
 	ret["users"] = pledge_executions.values("pledge__user").distinct().count()
 	ret["num_triggers"] = pledge_executions.values("trigger_execution").distinct().count()
-	ret["first_contrib_date"] = pledge_executions.order_by('created').first().created
-	ret["last_contrib_date"] = pledge_executions.order_by('created').last().created
+	if ret["num_triggers"] > 0:
+		ret["first_contrib_date"] = pledge_executions.order_by('created').first().created
+		ret["last_contrib_date"] = pledge_executions.order_by('created').last().created
 
 	# aggregate count and amount of campaign contributions
 	ret["total"] = ContributionAggregate.get_slice(**ca_slice_fields)

itfsite/templatetags/itfsite_utils.py
@@ -40,6 +40,7 @@ def currency(value):
 	# control over whether to show cents.
 
 	# value might be a Decimal instance.
+	if value == "": return "N/A"
 	value = float(value)
 
 	if value < 100: